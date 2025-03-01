fa947dcef4820741149b4030a3a31ec3bd55ed11
dhanraj0202 <dhanrajbeniwal34@gmail.com>
2025-03-01 23:42:43 +0530

    UC5_Put branch added

--- src/main/java/com/example/HelloWorld/controller/HelloWorldController.java
+++ src/main/java/com/example/HelloWorld/controller/HelloWorldController.java
@@ -10,21 +10,29 @@ public class HelloWorldController {
     public String sayHello(){
         return "Hello from Bridge Labz!";
     }
-
+    // Getting data from params
     @GetMapping("/query")
     public String sayHelloWithName(@RequestParam String name) {
         return "Hello " + name + " from BridgeLabz!";
     }
 
+    // getting data from Path/Url
     @GetMapping("/param/{name}")
     public String sayHelloWithPath(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz!";
     }
 
+    //Sending data using Post method
     @PostMapping("/post")
 
     public String getNameWithPostMethod(@RequestBody UserDTO user){
         return  "Hello "+user.getfirstName() +" "+user.getlastName()+" from Bridge labz";
+    }
+
+    //updating data using Put method
+    @PutMapping("/put/{firstName}")
+    public  String updateHelloGetFirstNameFromPathAndlastNamebyQueryUsingPut(@PathVariable String firstName, @RequestParam String lastName){
+        return "Hello "+firstName+ " " +lastName+ " from Bridge Labz";
     }
 
 }
